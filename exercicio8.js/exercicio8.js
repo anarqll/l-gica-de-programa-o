6cc2ccab8c239eefb39a2db1ad78cc899b7e8012
ana.macedo <raquelmacedo4222@gmail.com>
2026-05-07 20:39:08 -0300

    Calcule Salário por Hora

---
+++ exercicio8.js/exercicio8.js
@@ -0,0 +1,6 @@
+const valorPorHora = parseFloat(prompt("Quanto você ganha por hora?));
+const horasTrabalhadas = parseFloat(prompt("Quantas horas você trabalhou no mês?"));
+
+const salario = valorPorHora * horasTrabalhadas;
+
+document.write("Seu salário do mês é: R$ " + salario);
